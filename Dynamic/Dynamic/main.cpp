eb51d55eb4a606efa2f82c33ac71756ae78bb1b6
crupib <crupib@gmail.com>
2019-05-15 12:30:31 -0400

    dynamic array

---
+++ Dynamic/Dynamic/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include <vector> // for 2D vector
+
+using namespace std;
+
+
+// Complete the dynamicArray function below.
+vector<int> dynamicArray(int n, vector<vector<int>> queries) {
+    vector<int> myvector(n);
+    return myvector;
+}
+
+int main()
+{
+    int q=3;
+    vector<vector<int>> queries(q);
+}
