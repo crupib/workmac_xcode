1069046e7a20864cb2be2d0d183a1c1bc4644334
crupib <crupib@gmail.com>
2019-05-15 15:47:48 -0400

    Dynamic array day one

--- Dynamic/Dynamic/main.cpp
+++ Dynamic/Dynamic/main.cpp
@@ -8,11 +8,29 @@ using namespace std;
 // Complete the dynamicArray function below.
 vector<int> dynamicArray(int n, vector<vector<int>> queries) {
     vector<int> myvector(n);
+    int lastAnswer = 0;
+    int x,y = 0;
+    int seq = 0;
+    x = queries[0][0];
+    y = queries[0][1];
+    seq = ((x^lastAnswer)%n);
+    myvector[seq] = y;
+  //  cout << myvector[seq] << endl;
+    
+    
     return myvector;
 }
 
 int main()
 {
     int q=3;
+    vector<int> myvector(q);
     vector<vector<int>> queries(q);
+    queries[0].resize(3);
+    queries[0][0] = 2;
+    queries[0][1] = 5;
+    myvector = dynamicArray(3, queries);
+    cout << myvector[0] <<" " << myvector[1] << " " <<myvector[2]<< endl;
+    
+    
 }
