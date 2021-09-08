b9c6beaccdd15bf4d39faf283ce472d237802e5d
bill <crupib@gmail.com>
2021-09-08 15:56:00 -0400

    hello world, xcode

---
+++ hello/hello/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by William Crupi on 9/8/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    using namespace std;
+    // insert code here...
+    cout << "Hello, World of doucheness!\n";
+    int x = 8;
+    int y = 6;
+    cout << std::endl;
+    cout << x -y << " " << x * y << " " << x + y;
+    cout << std::endl;
+    return 0;
+}
